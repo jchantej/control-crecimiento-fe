04fe5cdb2065891b3464e5f235f6ed2dfde202f5
Juan Jimenez <jchantej@gmail.com>
2018-05-23 12:47:16 +0200

    lista persona para control de ccrecimiento

--- src/app/control-crecimiento/control-crecimiento.component.ts
+++ src/app/control-crecimiento/control-crecimiento.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { ControlCrecimiento } from './control-crecimiento.model';
 import { ControlCrecimientoService } from './control-crecimiento.service';
 import { MatSnackBar } from '@angular/material';
 
+import { Persona } from '../persona/persona.model';
+import { PersonaService } from '../persona/persona.service';
 
 @Component({
   selector: 'app-control-crecimiento',
@@ -12,37 +14,46 @@ import { MatSnackBar } from '@angular/material';
 })
 export class ControlCrecimientoComponent implements OnInit {
 
-
+  selectedPersona: Persona;
+  personas[]: Persona;
   controlForm: FormGroup;
+  idPersona: number;
+
   constructor(private formBuilder: FormBuilder,
     private controlCrecimientoService: ControlCrecimientoService,
+    private personaService: PersonaService,
     private snackBar: MatSnackBar) {
   }
   ngOnInit() {
-  //  this.sincronizarData();
+    this.getPersonas();
     this.createForm();
 
   }
 
-    createForm() {
-      this.controlForm = this.formBuilder.group({
-        peso: ['', Validators.required],
-        talla: ['', Validators.required]
-      });
-    }
+  getPersonas() {
+    this.personaService.getListaPersonas().subscribe(
+      persona => this.personas = persona
+    );
+  }
+  createForm() {
+    this.controlForm = this.formBuilder.group({
+      peso: ['', Validators.required],
+      talla: ['', Validators.required]
+    });
+  }
+
 
   private controlMapData(): ControlCrecimiento {
     const formModel = this.controlForm.value;
     const controlMap: ControlCrecimiento = {
       peso: formModel.peso,
       talla: formModel.talla,
-      idPersona: 203 //TODO:  Aqui se deb setear el valor del id de la perona
+      idPersona: this.selectedPersona.id //TODO:  Aqui se deb setear el valor del id de la perona
     };
     return controlMap;
   }
 
   public onSubmit() {
-
     if !(this.controlForm.invalid){
       this.controlCrecimientoService.crear(this.controlMapData()).subscribe(
         () => {
