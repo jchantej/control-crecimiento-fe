2f718486307a1919fa6772b9775b00b0354c0fb5
Juan Jimenez <jchantej@gmail.com>
2018-05-30 02:43:58 +0200

    Se agregan el percentil 15 y 85

--- src/app/percentil-oms/percentil-oms.component.ts
+++ src/app/percentil-oms/percentil-oms.component.ts
@@ -11,19 +11,11 @@ export class PercentilOmsComponent implements OnInit {
 
   controlesCrecimiento: any[];
   percentilesOms: PercentilOms[];
-  chartOptions = {
-    responsive: true,
+  options: {
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true,
-          stepValue: 5,
-        }
-      }],
-      xAxes: [{
-        ticks: {
-          steps: 100,
-          stepSize: 100
+          stepSize: 1
         }
       }]
     }
@@ -33,12 +25,28 @@ export class PercentilOmsComponent implements OnInit {
       data: [],
       label: 'Percentil 3',
       fill: false,
+      borderWidth: 1.5,
+      pointRadius: 0
+    },
+    {
+      data: [],
+      label: 'Percentil 15',
+      fill: false,
+      borderWidth: 1.5,
       pointRadius: 0
     },
     {
       data: [],
       label: 'Percentil 50',
       fill: false,
+      pointRadius: 0,
+      borderWidth: 1.5
+    },
+    {
+      data: [],
+      label: 'Percentil 85',
+      fill: false,
+      borderWidth: 1.5,
       pointRadius: 0
     },
     {
@@ -46,24 +54,67 @@ export class PercentilOmsComponent implements OnInit {
       label: 'Percentil 97',
       fill: false,
       pointRadius: 0,
+      borderWidth: 1.5
     },
     {
       data: [],
-      label: 'Nino',
+      label: '| Nin@',
       fill: false,
       showLine: false,
-      pointRadius: 10,
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-        '#000',
-        '#36A2EB',
-        '#FFCE56'
-      ]
+      pointRadius: 7.5
     }
+  ];
+
+  chartColors = [
+    {
+      backgroundColor: 'rgba(255,0,0,.6)',
+      borderColor: 'rgb(255,0,0)',
+      pointBackgroundColor: 'rgb(255,0,0',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(255,0,0,.8)'
+    },
+    {
+      backgroundColor: 'rgba(202, 111, 30, .1)',
+      borderColor: 'rgb(202, 111, 30)',
+      pointBackgroundColor: 'rgb(202, 111, 30)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(202, 111, 30,.8)'
+    },
+    {
+      backgroundColor: 'rgba(138,221,45,0.2)',
+      borderColor: 'rgb(138,221,45)',
+      pointBackgroundColor: 'rgb(138,221,45)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(138,221,45,0.2)'
+    },
+
+    {
+      backgroundColor: 'rgba(202, 111, 30, .1)',
+      borderColor: 'rgb(202, 111, 30)',
+      pointBackgroundColor: 'rgb(202, 111, 30)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(202, 111, 30,.8)'
+    },
+    {
+      backgroundColor: 'rgba(255,0,0,.6)',
+      borderColor: 'rgb(255,0,0)',
+      pointBackgroundColor: 'rgb(255,0,0',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(255,0,0,.8)'
+    },
+    {
+      backgroundColor: 'rgba(33, 33, 33,.1)',
+      borderColor: 'rgb(33, 33, 33)',
+      pointBackgroundColor: 'rgb(33, 33, 33)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(33, 33, 33, .8)'
+    },
   ];
 
   chartLabels = [];
@@ -78,7 +129,7 @@ export class PercentilOmsComponent implements OnInit {
   sincronizarData(controlesCrecimineto, persona, tipo) {
 
     let pivote = 0;
-    this.chartData[3].data = [];
+    this.chartData[5].data = [];
     const valoresX = [];
     const valoresY = [];
 
@@ -98,23 +149,24 @@ export class PercentilOmsComponent implements OnInit {
     this.percentilOmsService.getListaPercentilesOms(persona.genero, tipo.value, persona.edadDias).subscribe(
       percentil => {
         this.chartData[0].data = percentil.map(per => per.percentil3);
-        this.chartData[1].data = percentil.map(per => per.percentil50);
-        this.chartData[2].data = percentil.map(per => per.percentil97);
+        this.chartData[1].data = percentil.map(per => per.percentil15);
+        this.chartData[2].data = percentil.map(per => per.percentil50);
+        this.chartData[3].data = percentil.map(per => per.percentil85);
+        this.chartData[4].data = percentil.map(per => per.percentil97);
         this.chartLabels = percentil.map(per => per.edad.toString());
         for (let i = 0; i < valoresX.length; ++i) {
           for (let index = pivote; index < this.chartLabels.length; ++index) {
             if (this.chartLabels[index] === valoresX[i].toString()) {
-              this.chartData[3].data.push(valoresY[i]);
+              this.chartData[5].data.push(valoresY[i]);
               pivote++;
               break;
             }
-            this.chartData[3].data.push(null);
+            this.chartData[5].data.push(null);
             pivote++;
           }
         }
       });
   }
-
   onChartClick(event) {
     console.log(event);
   }
