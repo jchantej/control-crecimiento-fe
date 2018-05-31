96c93874581e931f15b611cdec86aa3faecf1ad3
Juan Jimenez <jchantej@gmail.com>
2018-05-31 12:53:38 +0200

    issue#6 Vista para registro usuario en inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
+import { Usuario } from '../usuario/usuario.model';
+
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario;
+  correo = new FormControl('', [Validators.required, Validators.email]);
+  registroForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    if (!this.usuario) {
+      this.usuario = { username: '', password: '', correo: '' };
+    }
+    this.createForm();
+  }
+
+  createForm() {
+    this.registroForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      correo: ['', Validators.required]
+    });
+  }
+
+
+  registrar() {
+    if (this.registroForm.valid) {
+      console.log(this.registroForm.value);
+      console.log('llamar el servicio de creacion de usuario');
+    } else {
+      console.log(this.usuario);
+      console.log('Mostrar toast con falta de datos');
+    }
+
   }
 
 }
