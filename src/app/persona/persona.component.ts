5a94740d209b5255a9828241e1e94696466eed25
Juan Jimenez <jchantej@gmail.com>
2018-06-05 02:55:07 +0200

    issue#9 paginacion y filtros tabla con listado ninios

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Persona } from './persona.model';
 import { PersonaService } from './persona.service';
-import { MatTableDataSource, MatDialogRef, MatDialog } from '@angular/material';
+import { MatTableDataSource, MatDialogRef, MatDialog, MatPaginator } from '@angular/material';
 import { PersonaCrudDialogComponent } from './dialogos/persona-crud-dialog.component';
 import { AuthService } from '../servicios/auth.service';
 @Component({
@@ -11,9 +11,11 @@ import { AuthService } from '../servicios/auth.service';
 })
 export class PersonaComponent implements OnInit {
   userSession: any;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSourcePersonas: MatTableDataSource<Persona>;
   displayedColumns = ['id', 'nombre', 'apellido', 'genero', 'fechaNacimiento', 'actionsColumn'];
   personaDialogRef: MatDialogRef<PersonaCrudDialogComponent>;
+
   constructor(private personaService: PersonaService,
     private personaDialogMat: MatDialog,
     private authService: AuthService) {
@@ -22,7 +24,7 @@ export class PersonaComponent implements OnInit {
   }
 
   ngOnInit() {
-   this.userSession = JSON.parse(localStorage.getItem(AuthService.tokenKey));
+    this.userSession = JSON.parse(localStorage.getItem(AuthService.tokenKey));
     //TODO:Pendiente de mejorar la parte control de la session
     if (this.userSession !== null) {
       this.authService.loggedIn.next(true);
@@ -36,9 +38,17 @@ export class PersonaComponent implements OnInit {
 
   sincronizarData() {
     this.personaService.getListaPersonas(this.userSession.id).subscribe(
-      persona => this.dataSourcePersonas = new MatTableDataSource<Persona>(persona)
-    );
+      persona => {
+        this.dataSourcePersonas = new MatTableDataSource<Persona>(persona);
+        this.dataSourcePersonas.paginator = this.paginator;
+      });
 
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSourcePersonas.filter = filterValue;
   }
 
   prepareCreate() {
@@ -46,11 +56,11 @@ export class PersonaComponent implements OnInit {
   }
 
   prepareUpdate(item) {
-    this.openDialog( item, 'UPDATE');
+    this.openDialog(item, 'UPDATE');
   }
 
   prepareDelete(item) {
-    this.openDialog( item, 'DELETE');
+    this.openDialog(item, 'DELETE');
   }
 
   openDialog(item?: number, action?: string) {
