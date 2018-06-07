8d0d784203b0404751012a0ac173cb4b567db834
Juan Jimenez <jchantej@gmail.com>
2018-06-07 12:06:38 +0200

    issue#11 mapeo de datos de usuario al form de editar

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../servicios/auth.service';
 import { Usuario } from '../usuario/usuario.model';
 import { UsuarioService } from '../usuario/usuario.service';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
@@ -11,10 +12,12 @@ import { UsuarioService } from '../usuario/usuario.service';
 export class UsuarioComponent implements OnInit {
   userSession: any;
   usuario: Usuario;
+  usuarioForm: FormGroup;
   constructor(private authService: AuthService,
-    private usuarioService: UsuarioService) { 
-      this.usuario = { username: '', password: '', correo: '' };
-    }
+    private usuarioService: UsuarioService,
+    private formBuilder: FormBuilder) {
+    this.usuario = { username: '', password: '', correo: '' };
+  }
 
   ngOnInit() {
     this.userSession = JSON.parse(localStorage.getItem(AuthService.tokenKey));
@@ -22,17 +25,36 @@ export class UsuarioComponent implements OnInit {
     if (this.userSession !== null) {
       this.authService.loggedIn.next(true);
       this.getUsuario();
+      this.createForm();
     } else {
       this.authService.logout();
     }
 
   }
 
+
+  createForm() {
+    console.log(this.usuario);
+    this.usuarioForm = this.formBuilder.group({
+      username: this.usuario.username ? this.usuario.username : '',
+      password: this.usuario.password ? this.usuario.password : '',
+      nombre: this.usuario.nombre ? this.usuario.nombre : '',
+      apellido: this.usuario.apellido ? this.usuario.apellido : '',
+      correo: this.usuario.correo ? this.usuario.correo : '',
+    });
+  }
+
+  tabActual(value) {
+    if (value.index === 1) {
+      this.createForm();
+    }
+
+  }
   getUsuario() {
     this.usuarioService.getUsuario(this.userSession.usuario).subscribe(
       u => {
         this.usuario = u;
-        console.log(this.usuario );
+        console.log(this.usuario);
       }
     );
   }
